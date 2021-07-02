8fbd72e10c92ef98937eafee06d5149ce55d0ea9
RicardoEsparza <riky.paramore@gmail.com>
2021-07-02 14:23:20 -0500

    Edición 1 del portfolio

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+sr.reveal('.infoMarca', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-200px'
+});
+
+sr.reveal('.foto', {
+    duration: 3000,
+    origin: 'left',
+    distance: '400px'
+});
+sr.reveal('.informacion', {
+    duration: 3000,
+    origin: 'right',
+    distance: '400px'
+});
+
+sr.reveal('.skill', {
+    duration: 3000
+});
+
+sr.reveal('.project', {
+    duration: 3000,
+    origin: 'left',
+    distance: '400px'
+});
+sr.reveal('.dproject', {
+    duration: 3000,
+    origin: 'right',
+    distance: '400px'
+});
